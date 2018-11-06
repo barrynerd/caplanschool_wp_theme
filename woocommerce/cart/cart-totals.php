f65fb443378a99fa11182f85181bfc1b2465523c
barry <bcaplan@gmail.com>
2018-11-06 15:26:47 -0800

    simplify button styling and format shopping cart

--- woocommerce/cart/cart-totals.php
+++ woocommerce/cart/cart-totals.php
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-<div class="col-md-5 cart_totals <?php echo ( WC()->customer->has_calculated_shipping() ) ? 'calculated_shipping' : ''; ?>">
+<div class="col-md-4 float-md-none offset-md-7 cart_totals <?php echo ( WC()->customer->has_calculated_shipping() ) ? 'calculated_shipping' : ''; ?>">
 
 	<?php do_action( 'woocommerce_before_cart_totals' ); ?>
 
