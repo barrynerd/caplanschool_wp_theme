960f151c42913f004c4097b3d06eb6f57a5ae70e
barry <bcaplan@gmail.com>
2018-11-14 12:08:19 -0800

    remove debugginig line

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -56,7 +56,6 @@ if ( post_password_required() ) {
 			 * @hooked woocommerce_template_single_sharing - 50
 			 * @hooked WC_Structured_Data::generate_product_data() - 60
 			 */
-			 echo "aaa";
 			do_action( 'woocommerce_single_product_summary' );
 			echo "bbb"
 		?>
