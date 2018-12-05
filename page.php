5af68635c6fc2af32fd94419937249e86b2430da
barry <bcaplan@gmail.com>
2018-12-04 16:26:26 -0800

    set margin on page template

--- page.php
+++ page.php
@@ -22,7 +22,7 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
 <div class="wrapper" id="page-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+	<div class="<?php echo esc_attr( $container ); ?> px-0" id="content" tabindex="-1">
 
 		<div class="row">
 
