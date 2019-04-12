28c60a83843e4a5f9e2fe4ca7b087b2ac0ec2842
barry <bcaplan@gmail.com>
2019-04-11 17:53:06 -0700

    first swag at dual menu

--- header-no_featured_img.php
+++ header-no_featured_img.php
@@ -47,29 +47,13 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<nav id="main-menu" class="navbar navbar-expand-md navbar-dark ">
+		<nav id="secondary-menu" class="navbar navbar-expand-md navbar-dark ">
 
 		<?php if ( 'container' == $container ) : ?>
 			<div class="container mx-0" >
 		<?php endif; ?>
 
-					<!-- Your site title as branding in the menu -->
-					<?php if ( ! has_custom_logo() ) { ?>
-
-						<?php if ( is_front_page() && is_home() ) : ?>
-
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
-
-						<?php else : ?>
-
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-
-						<?php endif; ?>
-
-
-					<?php } else {
-						the_custom_logo();
-					} ?><!-- end custom logo -->
+				<?php the_custom_logo();  ?>
 
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
 					<span class="navbar-toggler-icon"></span>
@@ -78,12 +62,13 @@ $container = get_theme_mod( 'understrap_container_type' );
 				<!-- The WordPress Menu goes here -->
 				<?php wp_nav_menu(
 					array(
-						'theme_location'  => 'primary',
+						'menu'			=> "bootstrap-menu02",
+						// 'theme_location'  => 'primary',
 						'container_class' => 'collapse navbar-collapse',
 						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav mr-auto align-items-center text-nowrap',
+						'menu_class'      => 'navbar-nav ml-auto align-items-center text-nowrap',
 						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
+						'menu_id'         => 'secondary-menu',
 						'depth'           => 2,
 						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
 					)
@@ -92,6 +77,21 @@ $container = get_theme_mod( 'understrap_container_type' );
 			</div><!-- .container -->
 			<?php endif; ?>
 
+		</nav><!-- .site-navigation -->
+
+		<nav id="main-menu" class="navbar navbar-expand-md navbar-dark ">
+			<?php wp_nav_menu(
+				array(
+					'theme_location'  => 'primary',
+					'container_class' => 'collapse navbar-collapse',
+					'container_id'    => 'navbarNavDropdown',
+					'menu_class'      => 'navbar-nav mr-auto align-items-center text-nowrap',
+					'fallback_cb'     => '',
+					'menu_id'         => 'main-menu',
+					'depth'           => 2,
+					'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+				)
+			); ?>
 		</nav><!-- .site-navigation -->
 
 	</div><!-- #wrapper-navbar end -->
