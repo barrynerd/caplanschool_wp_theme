70ea04d635876e95402b6ec9692b94d701b959e2
barry <bcaplan@gmail.com>
2019-04-12 18:46:55 -0700

    refactor menu code to it's own file

---
+++ template-parts/header-menu.php
@@ -0,0 +1,55 @@
+<!-- ******************* The Navbar Area ******************* -->
+<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite" class="row mx-0">
+
+    <a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
+
+    <div id="the-logo" class="col-1 px-0 pl-2">
+        <?php the_custom_logo();  ?>
+    </div>
+<div class="col-11 px-0">
+    <nav id="secondary-menu" class="navbar navbar-expand-md navbar-dark ">
+
+    <?php if ( 'container' == $container ) : ?>
+        <div class="container mx-0" >
+    <?php endif; ?>
+
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- The WordPress Menu goes here -->
+            <?php wp_nav_menu(
+                array(
+                    'menu'			=> "bootstrap-menu02a",
+                    // 'theme_location'  => 'primary',
+                    'container_class' => 'collapse navbar-collapse',
+                    'container_id'    => 'navbarNavDropdown',
+                    'menu_class'      => 'navbar-nav ml-auto align-items-center text-nowrap',
+                    'fallback_cb'     => '',
+                    // 'menu_id'         => 'secondary-menu',
+                    'depth'           => 2,
+                    'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+                )
+            ); ?>
+        <?php if ( 'container' == $container ) : ?>
+        </div><!-- .container -->
+        <?php endif; ?>
+
+    </nav><!-- .site-navigation -->
+
+    <nav id="main-menu" class="navbar navbar-expand-md navbar-dark ">
+        <?php wp_nav_menu(
+            array(
+                'theme_location'  => 'primary',
+                'container_class' => 'collapse navbar-collapse',
+                'container_id'    => 'navbarNavDropdown',
+                'menu_class'      => 'navbar-nav mr-auto align-items-center text-nowrap',
+                'fallback_cb'     => '',
+                'menu_id'         => 'main-menu',
+                'depth'           => 2,
+                'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+            )
+        ); ?>
+    </nav><!-- .site-navigation -->
+</div>
