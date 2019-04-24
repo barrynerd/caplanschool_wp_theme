f13740b2883bf38a900ced11c6be65fd03312102
barry <bcaplan@gmail.com>
2019-04-23 20:00:00 -0700

    clean up post meta display

--- functions.php
+++ functions.php
@@ -540,7 +540,7 @@ function understrap_posted_on()
                 'understrap_posted_on',
             sprintf(
                         '<span class="posted-on">%1$s <a href="%2$s" rel="bookmark">%3$s</a></span>',
-                        esc_html_x('Posted on', 'post date', 'understrap'),
+                        esc_html_x('Posted', 'post date', 'understrap'),
                         esc_url(get_permalink()),
                         apply_filters('understrap_posted_on_time', $time_string)
                 )
@@ -581,6 +581,38 @@ function my_home_category($query)
 }
 add_action('pre_get_posts', 'my_home_category');
 #----------------------------------------------------
+#override parent theme
+function understrap_entry_footer() {
+	// Hide category and tag text for pages.
+	if ( 'post' === get_post_type() ) {
+		/* translators: used between list items, there is a space after the comma */
+		$categories_list = get_the_category_list( esc_html__( ', ', 'understrap' ) );
+		if ( $categories_list && understrap_categorized_blog() ) {
+			/* translators: %s: Categories of current post */
+			printf( '<span class="cat-links">' . esc_html__( '%s', 'understrap' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+		}
+		/* translators: used between list items, there is a space after the comma */
+		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'understrap' ) );
+		if ( $tags_list ) {
+			/* translators: %s: Tags of current post */
+			printf( '<span class="tags-links">' . esc_html__( 'Tagged %s', 'understrap' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+		}
+	}
+	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		echo '<span class="comments-link">';
+		comments_popup_link( esc_html__( 'Leave a comment', 'understrap' ), esc_html__( '1 Comment', 'understrap' ), esc_html__( '% Comments', 'understrap' ) );
+		echo '</span>';
+	}
+	// edit_post_link(
+	// 	sprintf(
+	// 		/* translators: %s: Name of current post */
+	// 		esc_html__( 'Edit %s', 'understrap' ),
+	// 		the_title( '<span class="screen-reader-text">"', '"</span>', false )
+	// 	),
+	// 	'<span class="edit-link">',
+	// 	'</span>'
+	// );
+}
 #----------------------------------------------------
 #----------------------------------------------------
 #----------------------------------------------------
