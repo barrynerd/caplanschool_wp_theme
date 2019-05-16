9e6451caf287107d74db7b9932239e99a25652d3
barry <bcaplan@gmail.com>
2019-05-16 17:42:43 -0500

    bug 0004 - don't show meta data

---
+++ loop-templates/content-no-meta.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Search results partial template.
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+			<div class="card" >
+				<?php
+				if ( has_post_thumbnail() ) {
+					the_post_thumbnail('post-thumbnail',
+						['class' => 'card-img-top']);
+					}
+				echo theme_oembed_videos();
+				?>
+			  <div class="card-body">
+			    <h4 class="card-title">		<?php
+						the_title(
+							sprintf( '<a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+							'</a>'
+						);
+						?>
+				</h4>
+			    <p class="card-text entry-summary">
+					<?php the_excerpt(); ?>
+					</p>
+			  </div>
+
+		<?php endif; ?>
+
+</article><!-- #post-## -->
