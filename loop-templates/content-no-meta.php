038c3516f3b870ebfae8de5c55397381a15003d0
barry <bcaplan@gmail.com>
2019-06-04 13:17:51 -0700

    first swag at formatting ask elliot in browser

--- loop-templates/content-no-meta.php
+++ loop-templates/content-no-meta.php
@@ -5,34 +5,48 @@
  * @package understrap
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 ?>
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-		<?php if ( 'post' == get_post_type() ) : ?>
+		<?php if ('post' == get_post_type()) : ?>
 			<div class="card" >
 				<?php
-				if ( has_post_thumbnail() ) {
-					the_post_thumbnail('post-thumbnail',
-						['class' => 'card-img-top']);
-					}
-				// echo theme_oembed_videos();
-				?>
+                if (has_post_thumbnail()) {
+                    the_post_thumbnail(
+                        'post-thumbnail',
+                        ['class' => 'card-img-top']
+                    );
+                }
+                // echo theme_oembed_videos();
+                ?>
 			  <div class="card-body">
-			    <h4 class="card-title">		<?php
-						the_title(
-							sprintf( '<a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-							'</a>'
-						);
-						?>
+			    <h4 class="card-title">
+					<?php
+                        the_title(
+                            sprintf('<a href="%s" rel="bookmark">', esc_url(get_permalink())),
+                            '</a>'
+                        );
+                        ?>
 				</h4>
-				<?php echo theme_oembed_videos(); ?>
-			    <p class="card-text entry-summary">
-					<?php the_excerpt(); ?>
-					</p>
+				<div class="preview-wrapper">
+					<div class="col-5">
+						<?php
+						$my_embed = theme_oembed_videos();
+						print $my_embed;
+						?>
+					</div>
+					<div class="col-7 card-text entry-summary">
+						<?php
+						$my_excerpt = get_the_excerpt();
+						print $my_excerpt;
+						?>
+					</div>
+				</div>
+
 			  </div>
 
 		<?php endif; ?>
