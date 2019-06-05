33c7b00a0d387443e348b84e35b0b8fc87cf6ff7
barry <bcaplan@gmail.com>
2019-06-04 20:19:01 -0700

    parse all known youtube urls for ask elliot category page

--- functions.php
+++ functions.php
@@ -480,8 +480,12 @@ function theme_oembed_videos()
 
     if ($post && get_post($post)) {
         $result="";
-        $pattern="/(https:\/\/youtu.be\/.+?|http[s]*:\/\/www.youtube.com\/.*)/i";
-        // $pattern="/(http:\/\/www.youtube.com\/.*)/i";
+        $pattern="/(https:\/\/youtu.be\/[a-zA-Z0-9]*|http[s]*:\/\/www.youtube.com\/.*)/i";
+        $pattern1 ="https:\/\/youtu.be\/[a-zA-Z0-9]*";
+        $pattern2 ="(http[sv]*:\/\/www.youtube.com\/.+?)\&";
+        $pattern3 ="(http[sv]*:\/\/www.youtube.com\/watch\?v=[A-Za-z0-9_-]*)";
+
+        $regex = "/$pattern1|$pattern2|$pattern3/i";
         // if (current_user_can('administrator')) {
         //     print_r($pattern);
         //     print "<pre> Content:";
@@ -489,11 +493,31 @@ function theme_oembed_videos()
         //     print "</pre>";
         // }
 
-        if (preg_match($pattern, get_the_content(), $matches)) {
+        if (preg_match($regex, get_the_content(), $matches)) {
             // if (current_user_can('administrator')) {
+            //     print "Matches:";
             //     print_r($matches);
             // }
-            $embed = wp_oembed_get($matches[1], array('width'=>"100%", 'class'=>'embed-responsive-item'));
+
+            $my_embed = $matches[0];
+            if (!empty($matches[1])) {
+                $my_embed = $matches[1];
+            }
+            elseif (!empty($matches[2])) {
+                $my_embed = $matches[2];
+            }
+
+            $my_embed = str_replace("httpv:", "https:", $my_embed);
+            $my_embed = str_replace("http:", "https:", $my_embed);
+
+            $my_embed = str_replace("www.youtube.com/watch?v=", "youtu.be/", $my_embed);
+
+            // if (current_user_can('administrator')) {
+            //     print "my_embed:";
+            //     print $my_embed;
+            // }
+
+            $embed = wp_oembed_get($my_embed, array('width'=>"100%", 'class'=>'embed-responsive-item'));
             $result =<<<END
 				<div  class="embed-responsive embed-responsive-16by9">$embed</div>
 END;
