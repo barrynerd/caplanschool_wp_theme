f1da7529616bd1e10b35321357d2ad9574710e98
barry <bcaplan@gmail.com>
2019-06-12 19:11:31 -0700

    format and tweak excerpt display on search page

--- loop-templates/content-search.php
+++ loop-templates/content-search.php
@@ -5,8 +5,8 @@
  * @package understrap
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 ?>
 
@@ -15,13 +15,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<header class="entry-header">
 
 		<?php
-		the_title(
-			sprintf( '<h2 class="entry-title"><span><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-			'</a></span></h2>'
-		);
-		?>
+        the_title(
+            sprintf('<h2 class="entry-title"><span><a href="%s" rel="bookmark">', esc_url(get_permalink())),
+            '</a></span></h2>'
+        );
+        ?>
 
-		<?php if (False): #if ( 'post' == get_post_type() ) : ?>
+		<?php if (false): #if ( 'post' == get_post_type() ) :?>
 
 			<div class="entry-meta">
 
@@ -36,39 +36,33 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<div class="entry-summary">
 
 		<?php
-			$my_post_type = get_post_type();
-			// print $my_post_type;
+            $my_post_type = get_post_type();
+            if (current_user_can('administrator')) {
+                // print $my_post_type;
+            }
 
-			switch ($my_post_type) {
-				case "page": {
-					if (has_excerpt()) { #has custom excerpt
-						the_advanced_excerpt('length=50&length_type=words&no_custom=0&ellipsis=%26hellip;&exclude_tags=br,img,div,h4,p,iframed&read_more=');
-						}
-					else {
-						print "";
-					}
-					break;
-				}
-				case "post": {
-					the_advanced_excerpt('length=50&length_type=words&no_custom=1&ellipsis=%26hellip;&exclude_tags=br,img,div,h4,p,iframe&finish=word&read_more=');
-					break;
-				}
+            switch ($my_post_type) {
+                case "page": {
+                    the_advanced_excerpt('length=50&length_type=words&no_custom=0&ellipsis=%26hellip;&exclude_tags=ul,li,i,br,img,div,h1,h2,h3,h4,h5,h6,p,iframe,figure,figcaption,strong,em,a&finish=word&read_more=');
+                    break;
+                }
+                case "post": {
+                    the_advanced_excerpt('length=50&length_type=words&no_custom=1&ellipsis=%26hellip;&exclude_tags=ul,li,i,br,img,div,h1,h2,h3,h4,h5,h6,p,iframe,figure,figcaption,strong,em,a&finish=word&read_more=');
+                    break;
+                }
 
-				case "product":
-				default: {
-					the_advanced_excerpt('length=50&length_type=words&no_custom=1&ellipsis=%26hellip;&exclude_tags=br,img,div,h4,p,iframe&finish=word&read_more=');
-
-					// the_advanced_excerpt('length=10&length_type=words&no_custom=0&no_shortcode=0&ellipsis=%26hellip;&add_link=1&exclude_tags=h4,div p');
-					// the_excerpt();
-				}
-			}
+                case "product":
+                default: {
+                    the_advanced_excerpt('length=50&length_type=words&no_custom=1&ellipsis=%26hellip;&exclude_tags=ul,li,i,br,img,div,h1,h2,h3,h4,h5,h6,p,iframe,figure,figcaption,strong,em,a&finish=word&read_more=');
+                }
+            }
 
 
-			?>
+            ?>
 
 	</div><!-- .entry-summary -->
 
-<?php if (False): ?>
+<?php if (false): ?>
 	<footer class="entry-footer">
 
 		<?php understrap_entry_footer(); ?>
