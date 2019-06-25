998e5b805bfa7ffa79459a5b247f4c76e89ae713
barry <bcaplan@gmail.com>
2019-06-25 00:18:04 -0700

    allow sidebars to show on pages with page.php template

--- page.php
+++ page.php
@@ -24,6 +24,10 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
 	<div class="<?php echo esc_attr( $container ); ?> px-0" id="content" tabindex="-1">
 
+		<div class="row">
+
+			<!-- Do the left sidebar check -->
+			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
 
 			<main class="site-main" id="main">
 
@@ -35,6 +39,10 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
 			</main><!-- #main -->
 
+			<!-- Do the right sidebar check -->
+			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+
+		</div><!-- .row -->
 
 </div><!-- Container end -->
 
