4a40f2710257a3ee3e20f1900e2f61acdc52c14f
barry <bcaplan@gmail.com>
2019-06-26 15:04:49 -0700

    reformat neta data on single pages

--- functions.php
+++ functions.php
@@ -572,17 +572,16 @@ function understrap_posted_on()
     $posted_on   = apply_filters(
                 'understrap_posted_on',
             sprintf(
-                        '<div class="posted-on">%1$s <a href="%2$s" rel="bookmark">%3$s</a></div>',
-                        esc_html_x('Posted', 'post date', 'understrap'),
-                        esc_url(get_permalink()),
+                        '<span class="posted-on">%1$s %2$s</span>',
+                        esc_html_x('Posted on', 'post date', 'understrap'),
                         apply_filters('understrap_posted_on_time', $time_string)
                 )
         );
     $byline      = apply_filters(
                 'understrap_posted_by',
             sprintf(
-                        '<div class="byline"> %1$s<span class="author vcard"><a class="url fn n" href="%2$s"> %3$s</a></span></div>',
-                        $posted_on ? esc_html_x('By', 'post author', 'understrap') : esc_html_x('Posted by', 'post author', 'understrap'),
+                        '<span class="byline"> %1$s<span class="author vcard"><a class="url fn n" href="%2$s"> %3$s</a></span></span>',
+                        $posted_on ? esc_html_x('by', 'post author', 'understrap') : esc_html_x('Posted by', 'post author', 'understrap'),
                         esc_url(get_author_posts_url(get_the_author_meta('ID'))),
                         esc_html(get_the_author())
                 )
