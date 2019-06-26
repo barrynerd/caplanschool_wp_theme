2b3b724ec9c8a80d8dc7a34692ec561461b426cb
barry <bcaplan@gmail.com>
2019-06-26 15:23:22 -0700

    add meta data to ask elliott and industry news archive pages

--- category-ask-elliott.php
+++ category-ask-elliott.php
@@ -56,7 +56,7 @@ $container = get_theme_mod('understrap_container_type');
                          * If you want to override this in a child theme, then include a file
                          * called content-___.php (where ___ is the Post Format name) and that will be used instead.
                          */
-                        get_template_part('loop-templates/content-no-meta', get_post_format());
+                        get_template_part('loop-templates/content-meta', get_post_format());
                         ?>
 
 					<?php endwhile; ?>
