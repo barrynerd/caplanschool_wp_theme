2d3c739f4dad750c47c319a74e93d5934c823acf
barry <bcaplan@gmail.com>
2019-06-25 22:02:14 -0700

    creat no-sidebars template for checkout page

---
+++ page-no-sidebar.php
@@ -0,0 +1,50 @@
+<?php
+/* Template Name: Default,No Sidebars Template
+Template Post Type: post, page, product
+*/
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package understrap
+ */
+
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+get_header();
+
+$container   = get_theme_mod('understrap_container_type');
+
+?>
+
+<div class="wrapper" id="page-wrapper">
+
+	<div class="<?php echo esc_attr($container); ?> px-0" id="content" tabindex="-1">
+
+		<div class="row">
+
+
+			<main class="site-main" id="main">
+
+				<?php while (have_posts()) : the_post(); ?>
+
+					<?php get_template_part('loop-templates/content', 'page'); ?>
+
+				<?php endwhile; // end of the loop.?>
+
+			</main><!-- #main -->
+
+
+		</div><!-- .row -->
+
+</div><!-- Container end -->
+
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
