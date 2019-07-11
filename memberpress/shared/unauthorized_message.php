4fa4a8621a98bc637ec469bdaa07b9c01992653c
barry <bcaplan@gmail.com>
2019-07-10 19:45:49 -0700

    alertify reg form

--- memberpress/shared/unauthorized_message.php
+++ memberpress/shared/unauthorized_message.php
@@ -10,11 +10,11 @@
   </div>
 <?php endif; ?>
 <?php if(!MeprUtils::is_user_logged_in()): ?>
-  <div class="mepr-login-form-wrap">
+  <div class="mepr-login-form-wrap reg-form-alert alert alert-info col-3" role="alert">
   <?php if($show_login): ?>
     <?php echo $form; ?>
   <?php elseif(is_singular()): //Let's not show the annoying login link on non singular pages ?>
-    <span class="mepr-login-link">Already a member? <a href="<?php echo $mepr_options->login_page_url(); ?>"><?php echo MeprHooks::apply_filters('mepr-unauthorized-login-link-text',_x('Sign in', 'ui', 'memberpress')); ?></a></span>
+    <span class="mepr-login-link" >Already a member? <a href="<?php echo $mepr_options->login_page_url(); ?>"><?php echo MeprHooks::apply_filters('mepr-unauthorized-login-link-text',_x('Sign in', 'ui', 'memberpress')); ?></a></span>
   <?php endif; ?>
   </div>
 <?php endif; ?>
