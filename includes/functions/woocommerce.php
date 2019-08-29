d31e12a03ecaa3412314009bc8533cc8713ac040
barry <bcaplan@gmail.com>
2019-08-29 14:40:54 -0700

    no customized sale badge

--- includes/functions/woocommerce.php
+++ includes/functions/woocommerce.php
@@ -12,7 +12,9 @@ function wplook_activate_gutenberg_products($can_edit, $post_type){
 add_filter('use_block_editor_for_post_type', 'wplook_activate_gutenberg_products', 10, 2);
 
 #----------------------------------------------------
-// https://wordpress.org/support/topic/customizing-sale-badge-at-woocommerce/add_filter('woocommerce_sale_flash', 'my_custom_sales_badge');
+// https://wordpress.org/support/topic/customizing-sale-badge-at-woocommerce/
+
+// add_filter('woocommerce_sale_flash', 'my_custom_sales_badge');
 
 function my_custom_sales_badge() {
 	$img = '<img width="75px" height="30px" src="https://misofis.com/wp-content/uploads/indirim.gif"></img>';
