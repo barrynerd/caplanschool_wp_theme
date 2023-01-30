f5aefb5663a31232c5386bf94bcbd8caf1f96c2c
Barry Caplan <bcaplan@gmail.com>
2023-01-30 16:17:21 -0700

    first swag at shortcode to display courses byt category/title

--- includes/shortcodes/index.php
+++ includes/shortcodes/index.php
@@ -1,5 +1,6 @@
 <?php
 
 #echo dirname(__FILE__);
+require_once dirname(__FILE__) . '/src/bc_ce_classes_by_title.php';
 require_once dirname(__FILE__) . '/src/bc_ce_classes_by_month02.php';
 require_once dirname(__FILE__) . '/src/bc_members_classes_all.php';
