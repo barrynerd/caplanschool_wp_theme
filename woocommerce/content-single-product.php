cf3b1e9d0c59230aab12c27340d28ef399e2e2b3
Barry Caplan <bcaplan@gmail.com>
2023-11-28 15:33:51 -0700

    update to match latest woo template version

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -11,24 +11,27 @@
  * the readme will list any important changes.
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates
- * @version 3.4.0
+ * @package WooCommerce\Templates
+ * @version 3.6.0
  */
 
 defined( 'ABSPATH' ) || exit;
 
+global $product;
+
 /**
  * Hook: woocommerce_before_single_product.
  *
- * @hooked wc_print_notices - 10
+ * @hooked woocommerce_output_all_notices - 10
  */
 do_action( 'woocommerce_before_single_product' );
+
 if ( post_password_required() ) {
 	echo get_the_password_form(); // WPCS: XSS ok.
 	return;
 }
 ?>
-<div id="product-<?php the_ID(); ?>" <?php wc_product_class("product"); ?>>
+<div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
 
 	<?php
 			$image_size = array(740, 386);
@@ -36,54 +39,57 @@ if ( post_password_required() ) {
 	?>
 
 	<?php
-		// move actions so things display in the right order
-			// move price on single product page to be after the long product description
-			remove_action("woocommerce_single_product_summary", "woocommerce_template_single_price", 10);
-			add_action("woocommerce_after_single_product_summary", "woocommerce_template_single_price", 25);
+	/**
+	 * Hook: woocommerce_before_single_product_summary.
+	 *
+	 * @hooked woocommerce_show_product_sale_flash - 10
+	 * @hooked woocommerce_show_product_images - 20
+	 */
 
-			// move addtocart button on single product page to be after the long product description
-			remove_action("woocommerce_single_product_summary", "woocommerce_template_single_add_to_cart", 30);
-			add_action("woocommerce_after_single_product_summary", "woocommerce_template_single_add_to_cart", 25);
+	// move actions so things display in the right order
+	// move price on single product page to be after the long product description
+	remove_action("woocommerce_single_product_summary", "woocommerce_template_single_price", 10);
+	add_action("woocommerce_after_single_product_summary", "woocommerce_template_single_price", 25);
 
-			// remove the meta data from display
-			remove_action("woocommerce_single_product_summary","woocommerce_template_single_meta", 40);
+	// move addtocart button on single product page to be after the long product description
+	remove_action("woocommerce_single_product_summary", "woocommerce_template_single_add_to_cart", 30);
+	add_action("woocommerce_after_single_product_summary", "woocommerce_template_single_add_to_cart", 25);
+
+	// remove the meta data from display
+	remove_action("woocommerce_single_product_summary","woocommerce_template_single_meta", 40);
 
 	?>
+
 	<div class="short-description">
 		<?php
-			/**
-			 * Hook: woocommerce_single_product_summary.
-			 *
-			 * @hooked woocommerce_template_single_title - 5
-			 * @hooked woocommerce_template_single_rating - 10
-			 * @hooked woocommerce_template_single_price - 10
-			 * @hooked woocommerce_template_single_excerpt - 20
-			 * @hooked woocommerce_template_single_add_to_cart - 30
-			 * @hooked woocommerce_template_single_meta - 40
-			 * @hooked woocommerce_template_single_sharing - 50
-			 * @hooked WC_Structured_Data::generate_product_data() - 60
-			 */
-		
-			do_action( 'woocommerce_single_product_summary' );
-			
-			?>
+		/**
+		 * Hook: woocommerce_single_product_summary.
+		 *
+		 * @hooked woocommerce_template_single_title - 5
+		 * @hooked woocommerce_template_single_rating - 10
+		 * @hooked woocommerce_template_single_price - 10
+		 * @hooked woocommerce_template_single_excerpt - 20
+		 * @hooked woocommerce_template_single_add_to_cart - 30
+		 * @hooked woocommerce_template_single_meta - 40
+		 * @hooked woocommerce_template_single_sharing - 50
+		 * @hooked WC_Structured_Data::generate_product_data() - 60
+		 */
+		do_action( 'woocommerce_single_product_summary' );
+		?>
 	</div>
 
 	<div class="summary entry-summary">
-		<?php
 
-			/**
-			 * Hook: woocommerce_after_single_product_summary.
-			 *
-			 * @hooked woocommerce_output_product_data_tabs - 10
-			 * @hooked woocommerce_upsell_display - 15
-			 * @hooked woocommerce_output_related_products - 20
-			 */
-			// do_action( 'woocommerce_after_single_product_summary' );
-			
-			do_action( 'woocommerce_after_single_product_summary' );
-			// do_action( 'woocommerce_single_product_summary' );
-			?>
+	<?php
+	/**
+	 * Hook: woocommerce_after_single_product_summary.
+	 *
+	 * @hooked woocommerce_output_product_data_tabs - 10
+	 * @hooked woocommerce_upsell_display - 15
+	 * @hooked woocommerce_output_related_products - 20
+	 */
+	do_action( 'woocommerce_after_single_product_summary' );
+	?>
 	</div>
 
 </div>
