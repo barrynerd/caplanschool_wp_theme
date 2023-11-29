f48fd15439416668260ff2b3f68cd1a37391790f
Barry Caplan <bcaplan@gmail.com>
2023-11-28 18:13:20 -0700

    update to match latest version from woo templates

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -11,37 +11,54 @@
  * the readme will list any important changes.
  *
  * @see https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates
- * @version 3.4.0
+ * @package WooCommerce\Templates
+ * @version 7.0.1
  */
 
 defined( 'ABSPATH' ) || exit;
 
 global $product;
 
-#barry
-if (bcc_product_has_deposit($product)){
-    $button_string = esc_html( bcc_woo_custom_cart_button_text_deposit());
-    }
-else {
-    $button_string = esc_html( $product->add_to_cart_text());
-    }
+// barry - deprecated code related to deposits since we don't do that anymore
+// if (bcc_product_has_deposit($product)){
+//     $button_string = esc_html( bcc_woo_custom_cart_button_text_deposit());
+//     }
+// else {
+//     $button_string = esc_html( $product->add_to_cart_text());
+//     }
+
+$button_string = esc_html( $product->add_to_cart_text());
+
 
 if ( ! $product->is_purchasable() ) {
 	return;
 }
+
+// echo wc_get_stock_html( $product ); // WPCS: XSS ok.
 
 if ( $product->is_in_stock() ) : ?>
 
 	<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
 	<form class="cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data'>
-		<?php
-		// commented out to prevent yith deposits text
-		// do_action( 'woocommerce_before_add_to_cart_button' );
-		?>
+		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
-		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="btn btn-primary btn-block col-11 "><?php echo esc_html( $button_string ); ?></button>
+				<?php
+		do_action( 'woocommerce_before_add_to_cart_quantity' );
+
+		// woocommerce_quantity_input(
+		// 	array(
+		// 		'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
+		// 		'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
+		// 		'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+		// 	)
+		// );
+
+		// do_action( 'woocommerce_after_add_to_cart_quantity' );
+		// ?>
+
+
+		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="btn btn-primary btn-block col-11  single_add_to_cart_button alt<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>"><?php echo esc_html( $button_string ); ?></button>
 
 		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 	</form>
